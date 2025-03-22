89c71460cc7006b7f0d7ddb3c3e2ed8d1648924d
Espi Marisa <contact@espi.me>
2025-03-21 21:26:56 -0500

    chore: add basic page so i dont forgor

---
+++ scripts/http.py
@@ -0,0 +1,26 @@
+import http.server
+import socketserver
+import os
+
+
+PORT = 80
+
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Access-Control-Allow-Origin", "*")
+        super().end_headers()
+
+
+os.chdir("./landing/")
+handler_object = MyHttpRequestHandler
+with socketserver.TCPServer(("", PORT), handler_object) as httpd:
+    print(f"Serving at port {PORT}")
+    print("Server is running. Press Ctrl+C to stop the server.")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    print("Server stopped.")
+    httpd.server_close()
+    print("Server closed.")
