1c220221d01fba45e4dd776e302a233cc1d1920a
Espi Marisa <hi@espi.me>
2025-08-05 05:42:55 -0500

    feat: add back lidarr

--- scripts/docker-init.sh
+++ scripts/docker-init.sh
@@ -4,6 +4,7 @@
 docker volume create caddy-volume
 docker volume create gluetun-volume
 docker volume create jellyfin-volume
+docker volume create lidarr-volume
 docker volume create qbittorrent-volume
 docker volume create radarr-volume
 docker volume create sonarr-volume
