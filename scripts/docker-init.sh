3186cb27641636c8e8b09b5bd631d6d0e3dcf3cf
Espi Marisa <hi@espi.me>
2025-08-09 15:05:18 -0500

    chore: refactor stack; improve networking

--- scripts/docker-init.sh
+++ scripts/docker-init.sh
@@ -3,14 +3,16 @@
 # Creates volumes.
 docker volume create caddy-volume
 docker volume create gluetun-volume
+docker volume create jellyfin-cache-volume
 docker volume create jellyfin-volume
 docker volume create lidarr-volume
 docker volume create qbittorrent-volume
 docker volume create radarr-volume
 docker volume create sonarr-volume
-docker volume create uptime-kuma
 
 # Creates networks.
 docker network create -d bridge caddy-network
+docker network create -d bridge gluetun-network
+docker network create -d bridge media-network
 
 exit
