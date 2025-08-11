1523bca3af944fd5a14ffb215f87147f4bb6cd8e
Espi Marisa <hi@espi.me>
2025-08-10 22:59:54 -0500

    feat: add beszel, socket-proxy; stop using lsio + tag semvers

--- scripts/docker-init.sh
+++ scripts/docker-init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Creates volumes.
+docker volume create beszel-socket-volume
+docker volume create beszel-volume
 docker volume create caddy-volume
 docker volume create gluetun-volume
 docker volume create jellyfin-cache-volume
@@ -8,10 +10,12 @@ docker volume create jellyfin-volume
 docker volume create lidarr-volume
 docker volume create qbittorrent-volume
 docker volume create radarr-volume
+docker volume create socket-proxy-volume
 docker volume create sonarr-volume
 
 # Creates networks.
 docker network create -d bridge caddy-network
+docker network create -d bridge frontend-network
 docker network create -d bridge gluetun-network
 docker network create -d bridge media-network
 
