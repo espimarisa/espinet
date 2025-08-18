6be88c1c09e2eb67e4495b98a7471e553cbe5f64
Espi Marisa <hi@espi.me>
2025-08-17 22:27:24 -0500

    chore: update

--- scripts/docker-init.sh
+++ scripts/docker-init.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
 
 # Creates volumes.
-docker volume create beszel-socket-volume
-docker volume create beszel-volume
 docker volume create caddy-volume
 docker volume create gluetun-volume
 docker volume create jellyfin-cache-volume
 docker volume create jellyfin-volume
-docker volume create lidarr-volume
 docker volume create qbittorrent-volume
 docker volume create radarr-volume
 docker volume create socket-proxy-volume
 docker volume create sonarr-volume
-docker volume create uptime-kuma-volume
+docker volume create syncthing-volume
 
 # Creates networks.
-docker network create -d bridge caddy-network
-docker network create -d bridge frontend-network
+docker network create -d bridge external-network
+docker network create -d bridge internal-network
 docker network create -d bridge gluetun-network
-docker network create -d bridge media-network
 
 exit
