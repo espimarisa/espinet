0e440806869aaf9324ffc996ce3af1c54cac635f
Espi Marisa <hi@espi.me>
2025-09-17 02:49:36 -0500

    feat: add monitoring stack and cleanup services

--- scripts/docker-init.sh
+++ scripts/docker-init.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 # Creates networks.
-docker network create apps-network
-docker network create external-network
+docker network create caddy-network
 docker network create gluetun-network
-docker network create infrastructure-network
+docker network create infra-network
 
 # Creates volumes.
 docker volume create cleanuparr-volume
 docker volume create deemix-volume
+docker volume create dozzle-volume
 docker volume create gluetun-volume
+docker volume create jellyfin-cache-volume
 docker volume create jellyfin-volume
 docker volume create lidarr-volume
 docker volume create opencloud-volume
@@ -18,5 +19,8 @@ docker volume create prowlarr-volume
 docker volume create qbittorrent-volume
 docker volume create radarr-volume
 docker volume create readarr-volume
+docker volume create scrutiny-influxdb-volume
+docker volume create scrutiny-volume
 docker volume create sonarr-volume
+docker volume create uptime-kuma-volume
 docker volume create vaultwarden-volume
